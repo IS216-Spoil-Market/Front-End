ffbb65fe28cd9270ffed0b3d430ec5697e68e159
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-03 14:34:52 +0800

    Modify router to use RootLayout

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -4,11 +4,18 @@ import Chats from "../pages/Chats";
 import UserListing from "../pages/user/UserListing";
 import UserDetails from "../pages/user/UserDetails";
 import Profile from "../pages/Profile";
+import RootLayout from "../pages/RootLayout";
 
 export default createBrowserRouter([
     { path: "/", element: <Home /> },
-    { path: "/chat", element: <Chats /> },
     { path: "/user", element: <UserListing /> },
-    { path: "/user/:id", element: <UserDetails /> },
-    { path: "/profile", element: <Profile /> },
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { path: "/chat", element: <Chats /> },
+            { path: "/user/:id", element: <UserDetails /> },
+            { path: "/profile", element: <Profile /> },
+        ],
+    },
 ]);
