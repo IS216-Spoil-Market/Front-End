00bbd010cd2d6a88a7c6015c168cd7f056b037ed
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-03 10:55:51 +0800

    Create router for the application

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,4 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import Chats from "../pages/Chats";
+import UserListing from "../pages/user/UserListing";
+import UserDetails from "../pages/user/UserDetails";
+import Profile from "../pages/Profile";
 
-export default createBrowserRouter([{ path: "/", element: <Home /> }]);
+export default createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/chat", element: <Chats /> },
+    { path: "/user", element: <UserListing /> },
+    { path: "/user/:id", element: <UserDetails /> },
+    { path: "/profile", element: <Profile /> },
+]);
