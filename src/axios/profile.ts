d137712d733ca6493f2dffb10ecaff620fcb4e0d
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-27 16:57:01 +0800

    Add updateProfile axios function

--- src/axios/profile.ts
+++ src/axios/profile.ts
@@ -1,7 +1,21 @@
 import { Profile } from "../types/user";
+import { ProfileFormI } from "../zod-schema/profileSchema";
 import { profileInstance } from "./client";
 
 export const createOrGetUser = async () => {
     const { data } = await profileInstance.post("/");
+    return data as Profile;
+};
+
+export const updateProfile = async ({
+    skills_interested,
+    my_skills,
+    ...rest
+}: ProfileFormI) => {
+    const { data } = await profileInstance.put("/", {
+        skills_interested: skills_interested.map(({ value }) => value),
+        my_skills: my_skills.map(({ value }) => value),
+        ...rest,
+    });
     return data as Profile;
 };
