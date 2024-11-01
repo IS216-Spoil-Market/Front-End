ed5aa8fabcf7871eaafdbfe6f51b883d592f9152
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-11-01 21:57:32 +0800

    Create ChatToolbar for user to route to the user's profile and display current chat user's name

---
+++ src/components/chats/nav/ChatToolbar.tsx
@@ -0,0 +1,49 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import { Button, IconButton, Toolbar, Typography } from "@mui/material";
+import React, { SetStateAction } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface ChatToolBarProps {
+    open: boolean;
+    setOpen: (value: SetStateAction<boolean>) => void;
+    isAboveMedium: boolean;
+    activeChat: string;
+    receiverId: string;
+}
+
+const ChatToolBar: React.FC<ChatToolBarProps> = ({
+    open,
+    setOpen,
+    isAboveMedium,
+    activeChat,
+    receiverId,
+}) => {
+    const navigate = useNavigate()
+
+    return (
+        <Toolbar sx={{ display: "flex", direction: "row", justifyContent:"space-between" }}>
+            {!isAboveMedium && (
+                <IconButton
+                    onClick={() => setOpen(true)}
+                    edge="start"
+                    sx={[
+                        {
+                            mr: 2,
+                        },
+                        open && { display: "none" },
+                    ]}
+                >
+                    <MenuIcon sx={{ color: "black" }} />
+                </IconButton>
+            )}
+            <Typography variant="h6" noWrap component="div">
+                {activeChat}
+            </Typography>
+            <Button onClick={() => navigate(`/user/${receiverId}`)} color="primary" variant="contained">
+                View Profile
+            </Button>
+        </Toolbar>
+    );
+};
+
+export default ChatToolBar;
