307970d98034d236c2e98d6d9e94cc3b0959c08e
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-11-01 21:46:20 +0800

    Modify MessageInput to stick to bottom of the screen

--- src/components/chats/MessageInput.tsx
+++ src/components/chats/MessageInput.tsx
@@ -1,41 +1,44 @@
-import { Box, IconButton, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import { FormProvider, useForm } from "react-hook-form";
+import { Box, IconButton, TextField } from "@mui/material";
 import { useState } from "react";
+import { drawerWidth } from "../../constants/chatAppBar";
 
-interface MessageInputProps {}
+interface MessageInputProps {
+    open: boolean;
+}
 
-const MessageInput: React.FC<MessageInputProps> = ({}) => {
-	const formState = useForm();
-	const { handleSubmit, setValue, resetField } = formState;
-  const [message, setMessage] = useState("");
+const MessageInput: React.FC<MessageInputProps> = ({ open }) => {
+    const [message, setMessage] = useState("");
 
-	return (
-		<Box
-			position="sticky"
-			bottom={0}
-			sx={{
-				backgroundColor: "#FFFFFF",
-				padding: 2,
-				display: "flex",
-				flexDirection: "row",
-			}}
-		>
-			<FormProvider {...formState}>
-				<TextField
-					fullWidth
-					id="standard-basic"
-					placeholder="Type sth"
-					variant="standard"
-					onChange={({ target: { value } }) => {setValue("message", value); setMessage(value);}
-					}
-				/>
-				<IconButton disabled={message === "" ? true : false} onClick={handleSubmit((data) => console.log(data))}>
-					<SendIcon />
-				</IconButton>
-			</FormProvider>
-		</Box>
-	);
+    return (
+        <Box
+            sx={{
+                marginLeft: open ? `${drawerWidth}px` : 0,
+                width: `calc(100% - ${open ? drawerWidth : 0}px)`,
+                padding: 2,
+                display: "flex",
+                position: "fixed",
+                bgcolor: "white",
+                bottom: 0,
+                flexDirection: "row",
+            }}
+        >
+            <TextField
+                fullWidth
+                placeholder="Type something..."
+                variant="standard"
+                onChange={({ target: { value } }) => {
+                    setMessage(value);
+                }}
+            />
+            <IconButton
+                disabled={message === "" ? true : false}
+                onClick={() => console.log(message)}
+            >
+                <SendIcon />
+            </IconButton>
+        </Box>
+    );
 };
 
 export default MessageInput;
