00ded21fefacd26c01407da709818a10df600ae3
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-11-01 21:40:30 +0800

    Refactor chat bubble to render properly

--- src/components/chats/ChatBubble.tsx
+++ src/components/chats/ChatBubble.tsx
@@ -1,28 +1,46 @@
-import { ListItem, ListItemText } from "@mui/material";
+import {
+    Box,
+    ListItem,
+    Typography,
+    useMediaQuery,
+    useTheme,
+} from "@mui/material";
 
 interface ChatBubbleProps {
-	message: string;
-	type: "sender" | "receiver";
+    message: string;
+    type: "sender" | "receiver";
 }
 
 const ChatBubble: React.FC<ChatBubbleProps> = ({ message, type }) => {
-	const theme =
-		type === "sender" ? ["secondary", "start"] : ["primary", "end"];
+    const theme = useTheme();
+    const isAboveMedium = useMediaQuery(theme.breakpoints.up("md"));
+    const [variant, flex] =
+        type === "sender" ? ["secondary", "end"] : ["primary", "start"];
 
-	return (
-		<ListItem
-			sx={{
-				backgroundColor: `${theme[0]}.light`,
-				borderRadius: 16,
-				maxWidth: "50%",
-				margin: 2,
-				textAlign: `${theme[1]}`,
-				alignSelf: `flex-${theme[1]}`,
-			}}
-		>
-			<ListItemText>{message}</ListItemText>
-		</ListItem>
-	);
+    return (
+        <ListItem
+            sx={{
+                display: "flex",
+                direction: "row",
+                width: "100%",
+                justifyContent: flex,
+            }}
+        >
+            <Box
+                sx={{
+                    backgroundColor: `${variant}.light`,
+                    maxWidth: isAboveMedium ? "75%" : "90%",
+                    px: 2,
+                    py: 1,
+                    borderRadius: 16,
+                }}
+            >
+                <Typography color="white" display="inline-block" component="div">
+                    {message}
+                </Typography>
+            </Box>
+        </ListItem>
+    );
 };
 
 export default ChatBubble;
