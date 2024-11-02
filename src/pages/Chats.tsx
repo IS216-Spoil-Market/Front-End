d06cab6c1d92cba05558f79617719af3aa6835ae
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-11-02 17:15:02 +0800

    Modify useMediaQuery

--- src/pages/Chats.tsx
+++ src/pages/Chats.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Box, Drawer, useMediaQuery, useTheme } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import HeroBanner from "../assets/images/home/hero-banner.jpg";
+import { Drawer, Theme, useMediaQuery } from "@mui/material";
+import React, { useEffect, useMemo, useState } from "react";
+import ChatBackgroundContainer from "../components/chats/ChatBackgroundContainer";
 import ChatDisplay from "../components/chats/ChatDisplay";
 import ChatDrawerContents from "../components/chats/drawer/ChatDrawerContents";
 import MessageInput from "../components/chats/MessageInput";
@@ -16,124 +16,105 @@ interface ChatsProps {}
 
 const Chats: React.FC<ChatsProps> = ({}) => {
     // For styling purposes
-    const theme = useTheme();
-    const isAboveMedium = useMediaQuery(theme.breakpoints.up("md"));
+    const isAboveMedium = useMediaQuery((theme) =>
+        (theme as Theme).breakpoints.up("md")
+    );
     const offsetY = useScroll();
-    useEffect(() => {
-        isAboveMedium ? setOpen(true) : setOpen(false);
-    }, [isAboveMedium]);
 
     // Data fetching
     const { data: chats } = useChats();
     const { user } = useAuth0();
+
+    // For handling states
+    const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState("");
+
+    // Modify drawer state to default to open when screen is wide and close when narrow
+    useEffect(() => {
+        isAboveMedium ? setOpen(true) : setOpen(false);
+    }, [isAboveMedium]);
 
     // For keeping track of currently active chat
     useEffect(() => {
         if (chats && chats.length > 0) setSelected(chats[0].id);
     }, [chats]);
 
-    // For handling states
-    const [open, setOpen] = useState(false);
-    const [selected, setSelected] = useState("");
+    // Compute and find the user who is not the author based on each chat's users array
+    const [currentChat, receiver] = useMemo(() => {
+        const currentChat = chats?.find((chat) => chat.id === selected);
+        const receiver = currentChat?.users.find(
+            (chatUser) => chatUser.email !== user?.email
+        );
+
+        return [
+            currentChat,
+            { id: receiver?.id ?? "", name: receiver?.name ?? "Default" },
+        ];
+    }, [user, chats, selected]);
 
     return (
-        <Box
-            sx={{
-                height: `calc(100vh - 76px)`,
-            }}
-        >
-            <Box
-                width="100vw"
-                height="100vh"
-                sx={{ background: `url(${HeroBanner}) no-repeat fixed center` }}
+        <ChatBackgroundContainer>
+            <ChatAppbar
+                position="sticky"
+                sx={{ bgcolor: "white", color: "black" }}
+                open={open}
             >
-                <Box
-                    width="100vw"
-                    height="100vh"
-                    sx={{ background: "rgba(0,0,0,0.6)" }}
-                >
-                    <ChatAppbar
-                        position="sticky"
-                        sx={{ bgcolor: "white", color: "black" }}
-                        open={open}
-                    >
-                        <ChatToolBar
-                            {...{
-                                open,
-                                setOpen,
-                                isAboveMedium,
-                                // Can consider memoizing the current receiver and sender using useMemo for code clarity
-                                activeChat:
-                                    chats
-                                        ?.find((chat) => chat.id === selected)
-                                        ?.users.find(
-                                            (chatUser) =>
-                                                chatUser.email !== user?.email
-                                        )?.name ?? "Default",
-                                receiverId:
-                                    chats
-                                        ?.find((chat) => chat.id === selected)
-                                        ?.users.find(
-                                            (chatUser) =>
-                                                chatUser.email !== user?.email
-                                        )?.id ?? "",
-                            }}
-                        />
-                    </ChatAppbar>
+                <ChatToolBar
+                    {...{
+                        open,
+                        setOpen,
+                        isAboveMedium,
+                        activeChat: receiver.name,
+                        receiverId: receiver.id,
+                    }}
+                />
+            </ChatAppbar>
 
-                    {/**
-                     * The list of chats available
-                     * Note that this component cannot be moved into another file. It will break
-                     */}
-                    <Drawer
-                        sx={{
-                            width: drawerWidth,
-                            flexShrink: 0,
-                            "& .MuiDrawer-paper": {
-                                top: isAboveMedium
-                                    ? Math.max(76 - offsetY, 0)
-                                    : 0, // To make the drawer below the global top bar
-                                width: drawerWidth,
-                                boxSizing: "border-box",
-                            },
-                        }}
-                        variant={isAboveMedium ? "persistent" : "temporary"}
-                        anchor="left"
-                        open={open}
-                    >
-                        <ChatDrawerContents
-                            {...{
-                                setOpen,
-                                chats,
-                                isAboveMedium,
-                                selected,
-                                setSelected,
-                            }}
-                        />
-                    </Drawer>
+            {/**
+             * The list of chats available
+             * Note that this component cannot be moved into another file. It will break
+             */}
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    "& .MuiDrawer-paper": {
+                        top: isAboveMedium ? Math.max(76 - offsetY, 0) : 0, // To make the drawer below the global top bar
+                        width: drawerWidth,
+                        boxSizing: "border-box",
+                    },
+                }}
+                variant={isAboveMedium ? "persistent" : "temporary"}
+                anchor="left"
+                open={open}
+            >
+                <ChatDrawerContents
+                    {...{
+                        setOpen,
+                        chats,
+                        isAboveMedium,
+                        selected,
+                        setSelected,
+                    }}
+                />
+            </Drawer>
 
-                    {/* The main chatting section */}
-                    <ChatMain
-                        open={open}
-                        sx={{
-                            marginLeft: {
-                                xs: 0,
-                                md: open ? `${drawerWidth}px` : 0,
-                            },
-                            maxHeight: "100vh",
-                            overflowY: "scroll",
-                        }}
-                    >
-                        <ChatDisplay
-                            currentChat={chats?.find(
-                                (chat) => chat.id === selected
-                            )}
-                        />
-                    </ChatMain>
-                    <MessageInput open={open} />
-                </Box>
-            </Box>
-        </Box>
+            {/* The main chatting section */}
+            <ChatMain
+                open={open}
+                sx={{
+                    marginLeft: {
+                        xs: 0,
+                        md: open ? `${drawerWidth}px` : 0,
+                    },
+                    maxHeight: "100vh",
+                    overflowY: "scroll",
+                }}
+            >
+                <ChatDisplay currentChat={currentChat} />
+            </ChatMain>
+            <MessageInput chatId={selected} open={open} />
+        </ChatBackgroundContainer>
     );
 };
 
