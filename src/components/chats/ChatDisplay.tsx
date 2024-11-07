74bbb29356584767c9361656d75f9df6dd8fa78f
Poh En Xi <enxi.poh.2022@scis.smu.edu.sg>
2024-11-07 19:07:40 +0800

    added smooth scrolling to latest message when a new message is sent

--- src/components/chats/ChatDisplay.tsx
+++ src/components/chats/ChatDisplay.tsx
@@ -3,7 +3,7 @@ import { Box, List } from "@mui/material";
 import useMessages from "../../custom-hooks/react-query/chat/useMessagges";
 import { ChatItem } from "../../types/chat";
 import ChatBubble from "./ChatBubble";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { socket } from "../../constants/socket";
 
@@ -11,29 +11,32 @@ interface ChatDisplayProps {
 	currentChat?: ChatItem;
 }
 
-/* 
-    To-Do:
-    - change hardcoded endpoint?
-*/
-
 const ChatDisplay: React.FC<ChatDisplayProps> = ({ currentChat }) => {
 	const { data: messages } = useMessages(currentChat?.id);
 	const { user } = useAuth0();
 	const queryClient = useQueryClient();
+	const latestMessageRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-        //connect to socket in socket.ts
+		//connect to socket in socket.ts
 		socket.on("connected", () => console.log("socket connected!"));
-        //upon "message received" event, query for latest messages
+		//upon "message received" event, query for latest messages
 		socket.on("message received", (chatId) => {
 			queryClient.invalidateQueries({ queryKey: ["message", chatId] });
 		});
-        return () => {
-            // Clean up the socket listener
+		return () => {
+			// Clean up the socket listener
 			socket.off("connected");
-            socket.off("message received")
+			socket.off("message received");
 		};
 	});
+
+	useEffect(() => {
+		// Scroll to the latest message when `messages` change (new message received)
+		if (latestMessageRef.current) {
+			latestMessageRef.current.scrollIntoView({ behavior: "smooth" });
+		}
+	}, [messages]);
 
 	return (
 		<Box
@@ -41,6 +44,7 @@ const ChatDisplay: React.FC<ChatDisplayProps> = ({ currentChat }) => {
 				display: "flex",
 				flexDirection: "column-reverse",
 				minHeight: "100%",
+				pb: 10, //added som padding so the latest message doesn't get covered by the MessageInput component
 			}}
 		>
 			<List>
@@ -56,6 +60,7 @@ const ChatDisplay: React.FC<ChatDisplayProps> = ({ currentChat }) => {
 							}
 						/>
 					))}
+				<div ref={latestMessageRef}></div>
 			</List>
 		</Box>
 	);
